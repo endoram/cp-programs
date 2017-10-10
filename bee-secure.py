f8ef86ff7032086908ebffbee4bd48060f45fd53
Unknown <spencer.mcconnell2003@gmail.com>
2017-10-09 22:49:28 -0600

    Changes layout Added def pass_warn_age              def disable_guest_login              def enable_firewall

--- bee-secure.py
+++ bee-secure.py
@@ -25,23 +25,26 @@ os.chdir("/etc")
 subprocess.call(["sudo", "sed", "-i", '160s/.*/PASS_MAX_DAYS	35/', "login.defs"])
 print
 
-
 util.pass_max_days()
-
 print
 
 util.pass_min_days()
+print
 
+util.pass_warn_age()
+print
+
+util.disable_guest_login()
+print
+
+util.enable_firewall()
 print
 
 util.search_home()
-
 print
 
 util.search_user_home()
-
 print
 
 util.search_user_desktop()
-
 print
--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+
 import subprocess
 import time
 import os
@@ -10,26 +11,36 @@ def SleepTime():
 	time.sleep(1)
 
 def pass_max_days():
-	''' This fuction changes PASS_MAX_DAYS
-
-	'''
-	print("********************************")
-	print("***  Changing PASS_MAX_DAYS  ***")
-	print("********************************")
-
+	#This fuction changes PASS_MAX_DAYS to 35
+	print("Changing PASS_MAX_DAYS to 35")
 	subprocess.call(["sudo", "sed", "-i", '160s/.*/PASS_MAX_DAYS	35/', "login.defs"])
 	print("[Done]")
 	SleepTime()
 
 def pass_min_days():
-	#This fuction changes PASS_MIN_DAYS
-
-	print
-	print("********************************")
-	print("***  Changing PASS_MIN_DAYS  ***")
-	print("********************************")
+	#This fuction changes PASS_MIN_DAYS to 17
+	print("Changing PASS_MIN_DAYS to 17")
+	subprocess.call(["sudo", "sed", "-i", '161s/.*/PASS_MIN_DAYS	15/', "login.defs"])
 	print ("[Done]")
 
+def pass_warn_age():
+	#This function changes PASS_WARN_AGE to 7
+	print("Chaning PASS_WARN_AGE to 7")
+	subprocess.call(["sudo", "sed", "-i", '162s/.*/PASS_WARN_AGE	7/', "login.defs"])
+	print("[Done]")
+
+def disable_guest_login():
+	#This fuction diables the guest login
+	print("Disabling guest login")
+	os.chdir("/usr/share/lightdm/lightdm.conf.d")
+	subprocess.call(["sudo", "sed", "-i", '3s/.*/allow-guests=false/', "50-ubuntu.conf"])
+	print("[Done]")
+
+def enable_firewall():
+	#This funtion turns on the firewall
+	print("Enabling firewall")
+	subprocess.call(["sudo", "ufw", "enable"])
+	print("[Done]")
 
 #while
 #	try
